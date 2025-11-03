05ff50b0319196612aede665c09605217a191d08
Syed Musaeed Hossain <syedpunno@gmail.com>
2025-11-03 23:29:25 +0600

    first

---
+++ Rest_API_Practice_withDOTNET/Rest_API_Practice_withDOTNET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Rest_API_Practice_withDOTNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
