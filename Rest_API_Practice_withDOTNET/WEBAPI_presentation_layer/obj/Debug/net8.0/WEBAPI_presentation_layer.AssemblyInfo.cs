122b6387f40d655384520dc30c2f0fb493ed9e78
Syed Musaeed Hossain <syedpunno@gmail.com>
2025-11-09 02:16:52 +0600

    Changed the reference of the BL Layer.

--- Rest_API_Practice_withDOTNET/WEBAPI_presentation_layer/obj/Debug/net8.0/WEBAPI_presentation_layer.AssemblyInfo.cs
+++ Rest_API_Practice_withDOTNET/WEBAPI_presentation_layer/obj/Debug/net8.0/WEBAPI_presentation_layer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBAPI_presentation_layer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05ff50b0319196612aede665c09605217a191d08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+711e186441e847375121776eb3a1bdc0a9884cd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBAPI_presentation_layer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBAPI_presentation_layer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
